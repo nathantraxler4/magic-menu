8376c417d9b8e138f5e13cc70a59dfa7d43cb5e9
Nathan Traxler <nathantraxler@Nathans-MacBook-Pro.local>
2024-09-19 08:07:01 -0700

    Move selected state from RecipeSelectBox to RecipeBook

--- app/components/RecipeSelectBox.tsx
+++ app/components/RecipeSelectBox.tsx
@@ -1,27 +1,31 @@
-import React, { useState, useCallback } from 'react'
+import React from 'react'
 import { Text, StyleSheet, TouchableHighlight } from 'react-native'
 import colors from '../styles/colors'
 import { selectableStyles } from '../styles/selectable'
 
-export const RecipeSelectBox = (props: { dishName: string }) => {
-    const [isSelected, setIsSelected] = useState(false)
-
-    const toggleSelect = useCallback(() => {
-        setIsSelected(!isSelected)
-    }, [isSelected])
+export const RecipeSelectBox = (props: {
+    dishName: string
+    selected: Set<string>
+    toggleSelected: (dishName: string) => void
+}) => {
+    const isThisSelected = props.selected.has(props.dishName)
 
     const styles = StyleSheet.create({
         selectable: {
             ...selectableStyles.selectableContainer,
-            borderColor: isSelected ? colors.accent : colors.lighterAccent
+            borderColor: isThisSelected ? colors.accent : colors.lighterAccent
         },
         selectableText: {
             ...selectableStyles.textCenter
         }
     })
 
+    const onPressHandler = () => {
+        props.toggleSelected(props.dishName)
+    }
+
     return (
-        <TouchableHighlight onPress={toggleSelect} style={styles.selectable}>
+        <TouchableHighlight onPress={onPressHandler} style={styles.selectable}>
             <Text style={styles.selectableText}>{props.dishName}</Text>
         </TouchableHighlight>
     )
--- app/screens/RecipeBook.tsx
+++ app/screens/RecipeBook.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { View, StyleSheet, FlatList } from 'react-native'
 
 import Recipe from '../models/Recipe'
@@ -8,13 +8,33 @@ import { RecipeSelectBox } from '../components/RecipeSelectBox'
 
 export const RecipeBook = () => {
     const recipes: Realm.Results<Recipe> = useQuery(Recipe)
+    const [selected, setSelected] = useState(new Set<string>())
+
+    const toggleSelected = useCallback(
+        (dishName) => {
+            if (selected.has(dishName)) {
+                const selectedCopy = new Set<string>(selected)
+                selectedCopy.delete(dishName)
+                setSelected(selectedCopy)
+            } else {
+                setSelected(new Set([...selected, dishName]))
+            }
+        },
+        [selected]
+    )
 
     return (
         <View style={styles.container}>
             <FlatList
                 data={recipes}
                 keyExtractor={(recipe) => recipe.name}
-                renderItem={({ item }) => <RecipeSelectBox dishName={item.name} />}
+                renderItem={({ item }) => (
+                    <RecipeSelectBox
+                        dishName={item.name}
+                        selected={selected}
+                        toggleSelected={toggleSelected}
+                    />
+                )}
             />
         </View>
     )
