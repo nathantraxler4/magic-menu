38065ee5b24549032a92eef0cc958ea9a2d63e6f
Nathan Traxler <nathantraxler@Nathans-MacBook-Pro.local>
2024-10-10 11:40:16 -0700

    Remove code from AtlasDeviceSDK Template

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ import 'expo-dev-client'
 import 'react-native-get-random-values'
 import React from 'react'
 import { registerRootComponent } from 'expo'
-import { AppWrapperNonSync } from './app/AppWrapperNonSync'
-import { AppWrapperSync } from './app/AppWrapperSync'
-import { SYNC_CONFIG } from './sync.config'
+import { AppWrapperNonSync } from '@app/AppWrapperNonSync'
 
-const App = () =>
-    SYNC_CONFIG.enabled ? <AppWrapperSync appId={SYNC_CONFIG.appId} /> : <AppWrapperNonSync />
+const App = () => <AppWrapperNonSync />
 
 registerRootComponent(App)
