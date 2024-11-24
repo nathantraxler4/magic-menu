2597396607c6f0fd5300edd0682406b69fbc401d
Nathan Traxler <nathantraxler@MacBook-Pro.local>
2024-11-23 17:28:28 -0700

    Ignore generated GraphQL code in .prettierignore

--- app/__generated__/gql.ts
+++ app/__generated__/gql.ts
@@ -14,14 +14,10 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
  */
 const documents = {
-    '\n    mutation AddRecipes($recipes: [RecipeInput!]!) {\n        addRecipes(recipes: $recipes) {\n            name\n            ingredients\n            directions\n        }\n    }\n':
-        types.AddRecipesDocument,
-    '\n    query Recipes {\n        recipes {\n            name\n            ingredients\n            directions\n        }\n    }\n':
-        types.RecipesDocument,
-    '\n    query Menus {\n        menus {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n':
-        types.MenusDocument,
-    '\n    query GenerateMenu($recipes: [RecipeInput!]!) {\n        generateMenu(recipes: $recipes) {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n':
-        types.GenerateMenuDocument
+    "\n    mutation AddRecipes($recipes: [RecipeInput!]!) {\n        addRecipes(recipes: $recipes) {\n            name\n            ingredients\n            directions\n        }\n    }\n": types.AddRecipesDocument,
+    "\n    query Recipes {\n        recipes {\n            name\n            ingredients\n            directions\n        }\n    }\n": types.RecipesDocument,
+    "\n    query Menus {\n        menus {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n": types.MenusDocument,
+    "\n    query GenerateMenu($recipes: [RecipeInput!]!) {\n        generateMenu(recipes: $recipes) {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n": types.GenerateMenuDocument,
 };
 
 /**
@@ -41,31 +37,22 @@ export function gql(source: string): unknown;
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-    source: '\n    mutation AddRecipes($recipes: [RecipeInput!]!) {\n        addRecipes(recipes: $recipes) {\n            name\n            ingredients\n            directions\n        }\n    }\n'
-): (typeof documents)['\n    mutation AddRecipes($recipes: [RecipeInput!]!) {\n        addRecipes(recipes: $recipes) {\n            name\n            ingredients\n            directions\n        }\n    }\n'];
+export function gql(source: "\n    mutation AddRecipes($recipes: [RecipeInput!]!) {\n        addRecipes(recipes: $recipes) {\n            name\n            ingredients\n            directions\n        }\n    }\n"): (typeof documents)["\n    mutation AddRecipes($recipes: [RecipeInput!]!) {\n        addRecipes(recipes: $recipes) {\n            name\n            ingredients\n            directions\n        }\n    }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-    source: '\n    query Recipes {\n        recipes {\n            name\n            ingredients\n            directions\n        }\n    }\n'
-): (typeof documents)['\n    query Recipes {\n        recipes {\n            name\n            ingredients\n            directions\n        }\n    }\n'];
+export function gql(source: "\n    query Recipes {\n        recipes {\n            name\n            ingredients\n            directions\n        }\n    }\n"): (typeof documents)["\n    query Recipes {\n        recipes {\n            name\n            ingredients\n            directions\n        }\n    }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-    source: '\n    query Menus {\n        menus {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n'
-): (typeof documents)['\n    query Menus {\n        menus {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n'];
+export function gql(source: "\n    query Menus {\n        menus {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n"): (typeof documents)["\n    query Menus {\n        menus {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-    source: '\n    query GenerateMenu($recipes: [RecipeInput!]!) {\n        generateMenu(recipes: $recipes) {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n'
-): (typeof documents)['\n    query GenerateMenu($recipes: [RecipeInput!]!) {\n        generateMenu(recipes: $recipes) {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n'];
+export function gql(source: "\n    query GenerateMenu($recipes: [RecipeInput!]!) {\n        generateMenu(recipes: $recipes) {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n"): (typeof documents)["\n    query GenerateMenu($recipes: [RecipeInput!]!) {\n        generateMenu(recipes: $recipes) {\n            courses {\n                name\n                description\n            }\n            backgroundImage\n        }\n    }\n"];
 
 export function gql(source: string) {
-    return (documents as any)[source] ?? {};
+  return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
-    TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
--- app/__generated__/graphql.ts
+++ app/__generated__/graphql.ts
@@ -5,309 +5,90 @@ export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = {
-    [_ in K]?: never;
-};
-export type Incremental<T> =
-    | T
-    | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-    ID: { input: string; output: string };
-    String: { input: string; output: string };
-    Boolean: { input: boolean; output: boolean };
-    Int: { input: number; output: number };
-    Float: { input: number; output: number };
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
 };
 
 export type Course = {
-    __typename?: 'Course';
-    description: Scalars['String']['output'];
-    name: Scalars['String']['output'];
+  __typename?: 'Course';
+  description: Scalars['String']['output'];
+  name: Scalars['String']['output'];
 };
 
 export type Menu = {
-    __typename?: 'Menu';
-    backgroundImage: Scalars['String']['output'];
-    courses: Array<Course>;
+  __typename?: 'Menu';
+  backgroundImage: Scalars['String']['output'];
+  courses: Array<Course>;
 };
 
 export type Mutation = {
-    __typename?: 'Mutation';
-    addRecipes: Array<Recipe>;
+  __typename?: 'Mutation';
+  addRecipes: Array<Recipe>;
 };
 
+
 export type MutationAddRecipesArgs = {
-    recipes: Array<RecipeInput>;
+  recipes: Array<RecipeInput>;
 };
 
 export type Query = {
-    __typename?: 'Query';
-    generateMenu: Menu;
-    menus?: Maybe<Array<Maybe<Menu>>>;
-    recipes: Array<Recipe>;
+  __typename?: 'Query';
+  generateMenu: Menu;
+  menus?: Maybe<Array<Maybe<Menu>>>;
+  recipes: Array<Recipe>;
 };
 
+
 export type QueryGenerateMenuArgs = {
-    recipes: Array<RecipeInput>;
+  recipes: Array<RecipeInput>;
 };
 
 export type Recipe = {
-    __typename?: 'Recipe';
-    directions: Scalars['String']['output'];
-    ingredients: Scalars['String']['output'];
-    name: Scalars['String']['output'];
+  __typename?: 'Recipe';
+  directions: Scalars['String']['output'];
+  ingredients: Scalars['String']['output'];
+  name: Scalars['String']['output'];
 };
 
 export type RecipeInput = {
-    directions: Scalars['String']['input'];
-    ingredients: Scalars['String']['input'];
-    name: Scalars['String']['input'];
+  directions: Scalars['String']['input'];
+  ingredients: Scalars['String']['input'];
+  name: Scalars['String']['input'];
 };
 
 export type AddRecipesMutationVariables = Exact<{
-    recipes: Array<RecipeInput> | RecipeInput;
+  recipes: Array<RecipeInput> | RecipeInput;
 }>;
 
-export type AddRecipesMutation = {
-    __typename?: 'Mutation';
-    addRecipes: Array<{
-        __typename?: 'Recipe';
-        name: string;
-        ingredients: string;
-        directions: string;
-    }>;
-};
 
-export type RecipesQueryVariables = Exact<{ [key: string]: never }>;
+export type AddRecipesMutation = { __typename?: 'Mutation', addRecipes: Array<{ __typename?: 'Recipe', name: string, ingredients: string, directions: string }> };
 
-export type RecipesQuery = {
-    __typename?: 'Query';
-    recipes: Array<{
-        __typename?: 'Recipe';
-        name: string;
-        ingredients: string;
-        directions: string;
-    }>;
-};
+export type RecipesQueryVariables = Exact<{ [key: string]: never; }>;
 
-export type MenusQueryVariables = Exact<{ [key: string]: never }>;
 
-export type MenusQuery = {
-    __typename?: 'Query';
-    menus?: Array<{
-        __typename?: 'Menu';
-        backgroundImage: string;
-        courses: Array<{ __typename?: 'Course'; name: string; description: string }>;
-    } | null> | null;
-};
+export type RecipesQuery = { __typename?: 'Query', recipes: Array<{ __typename?: 'Recipe', name: string, ingredients: string, directions: string }> };
+
+export type MenusQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MenusQuery = { __typename?: 'Query', menus?: Array<{ __typename?: 'Menu', backgroundImage: string, courses: Array<{ __typename?: 'Course', name: string, description: string }> } | null> | null };
 
 export type GenerateMenuQueryVariables = Exact<{
-    recipes: Array<RecipeInput> | RecipeInput;
+  recipes: Array<RecipeInput> | RecipeInput;
 }>;
 
-export type GenerateMenuQuery = {
-    __typename?: 'Query';
-    generateMenu: {
-        __typename?: 'Menu';
-        backgroundImage: string;
-        courses: Array<{ __typename?: 'Course'; name: string; description: string }>;
-    };
-};
 
-export const AddRecipesDocument = {
-    kind: 'Document',
-    definitions: [
-        {
-            kind: 'OperationDefinition',
-            operation: 'mutation',
-            name: { kind: 'Name', value: 'AddRecipes' },
-            variableDefinitions: [
-                {
-                    kind: 'VariableDefinition',
-                    variable: { kind: 'Variable', name: { kind: 'Name', value: 'recipes' } },
-                    type: {
-                        kind: 'NonNullType',
-                        type: {
-                            kind: 'ListType',
-                            type: {
-                                kind: 'NonNullType',
-                                type: {
-                                    kind: 'NamedType',
-                                    name: { kind: 'Name', value: 'RecipeInput' }
-                                }
-                            }
-                        }
-                    }
-                }
-            ],
-            selectionSet: {
-                kind: 'SelectionSet',
-                selections: [
-                    {
-                        kind: 'Field',
-                        name: { kind: 'Name', value: 'addRecipes' },
-                        arguments: [
-                            {
-                                kind: 'Argument',
-                                name: { kind: 'Name', value: 'recipes' },
-                                value: {
-                                    kind: 'Variable',
-                                    name: { kind: 'Name', value: 'recipes' }
-                                }
-                            }
-                        ],
-                        selectionSet: {
-                            kind: 'SelectionSet',
-                            selections: [
-                                { kind: 'Field', name: { kind: 'Name', value: 'name' } },
-                                { kind: 'Field', name: { kind: 'Name', value: 'ingredients' } },
-                                { kind: 'Field', name: { kind: 'Name', value: 'directions' } }
-                            ]
-                        }
-                    }
-                ]
-            }
-        }
-    ]
-} as unknown as DocumentNode<AddRecipesMutation, AddRecipesMutationVariables>;
-export const RecipesDocument = {
-    kind: 'Document',
-    definitions: [
-        {
-            kind: 'OperationDefinition',
-            operation: 'query',
-            name: { kind: 'Name', value: 'Recipes' },
-            selectionSet: {
-                kind: 'SelectionSet',
-                selections: [
-                    {
-                        kind: 'Field',
-                        name: { kind: 'Name', value: 'recipes' },
-                        selectionSet: {
-                            kind: 'SelectionSet',
-                            selections: [
-                                { kind: 'Field', name: { kind: 'Name', value: 'name' } },
-                                { kind: 'Field', name: { kind: 'Name', value: 'ingredients' } },
-                                { kind: 'Field', name: { kind: 'Name', value: 'directions' } }
-                            ]
-                        }
-                    }
-                ]
-            }
-        }
-    ]
-} as unknown as DocumentNode<RecipesQuery, RecipesQueryVariables>;
-export const MenusDocument = {
-    kind: 'Document',
-    definitions: [
-        {
-            kind: 'OperationDefinition',
-            operation: 'query',
-            name: { kind: 'Name', value: 'Menus' },
-            selectionSet: {
-                kind: 'SelectionSet',
-                selections: [
-                    {
-                        kind: 'Field',
-                        name: { kind: 'Name', value: 'menus' },
-                        selectionSet: {
-                            kind: 'SelectionSet',
-                            selections: [
-                                {
-                                    kind: 'Field',
-                                    name: { kind: 'Name', value: 'courses' },
-                                    selectionSet: {
-                                        kind: 'SelectionSet',
-                                        selections: [
-                                            {
-                                                kind: 'Field',
-                                                name: { kind: 'Name', value: 'name' }
-                                            },
-                                            {
-                                                kind: 'Field',
-                                                name: { kind: 'Name', value: 'description' }
-                                            }
-                                        ]
-                                    }
-                                },
-                                { kind: 'Field', name: { kind: 'Name', value: 'backgroundImage' } }
-                            ]
-                        }
-                    }
-                ]
-            }
-        }
-    ]
-} as unknown as DocumentNode<MenusQuery, MenusQueryVariables>;
-export const GenerateMenuDocument = {
-    kind: 'Document',
-    definitions: [
-        {
-            kind: 'OperationDefinition',
-            operation: 'query',
-            name: { kind: 'Name', value: 'GenerateMenu' },
-            variableDefinitions: [
-                {
-                    kind: 'VariableDefinition',
-                    variable: { kind: 'Variable', name: { kind: 'Name', value: 'recipes' } },
-                    type: {
-                        kind: 'NonNullType',
-                        type: {
-                            kind: 'ListType',
-                            type: {
-                                kind: 'NonNullType',
-                                type: {
-                                    kind: 'NamedType',
-                                    name: { kind: 'Name', value: 'RecipeInput' }
-                                }
-                            }
-                        }
-                    }
-                }
-            ],
-            selectionSet: {
-                kind: 'SelectionSet',
-                selections: [
-                    {
-                        kind: 'Field',
-                        name: { kind: 'Name', value: 'generateMenu' },
-                        arguments: [
-                            {
-                                kind: 'Argument',
-                                name: { kind: 'Name', value: 'recipes' },
-                                value: {
-                                    kind: 'Variable',
-                                    name: { kind: 'Name', value: 'recipes' }
-                                }
-                            }
-                        ],
-                        selectionSet: {
-                            kind: 'SelectionSet',
-                            selections: [
-                                {
-                                    kind: 'Field',
-                                    name: { kind: 'Name', value: 'courses' },
-                                    selectionSet: {
-                                        kind: 'SelectionSet',
-                                        selections: [
-                                            {
-                                                kind: 'Field',
-                                                name: { kind: 'Name', value: 'name' }
-                                            },
-                                            {
-                                                kind: 'Field',
-                                                name: { kind: 'Name', value: 'description' }
-                                            }
-                                        ]
-                                    }
-                                },
-                                { kind: 'Field', name: { kind: 'Name', value: 'backgroundImage' } }
-                            ]
-                        }
-                    }
-                ]
-            }
-        }
-    ]
-} as unknown as DocumentNode<GenerateMenuQuery, GenerateMenuQueryVariables>;
+export type GenerateMenuQuery = { __typename?: 'Query', generateMenu: { __typename?: 'Menu', backgroundImage: string, courses: Array<{ __typename?: 'Course', name: string, description: string }> } };
+
+
+export const AddRecipesDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"AddRecipes"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"recipes"}},"type":{"kind":"NonNullType","type":{"kind":"ListType","type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"RecipeInput"}}}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"addRecipes"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"recipes"},"value":{"kind":"Variable","name":{"kind":"Name","value":"recipes"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"ingredients"}},{"kind":"Field","name":{"kind":"Name","value":"directions"}}]}}]}}]} as unknown as DocumentNode<AddRecipesMutation, AddRecipesMutationVariables>;
+export const RecipesDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Recipes"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"recipes"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"ingredients"}},{"kind":"Field","name":{"kind":"Name","value":"directions"}}]}}]}}]} as unknown as DocumentNode<RecipesQuery, RecipesQueryVariables>;
+export const MenusDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"Menus"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"menus"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"courses"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"description"}}]}},{"kind":"Field","name":{"kind":"Name","value":"backgroundImage"}}]}}]}}]} as unknown as DocumentNode<MenusQuery, MenusQueryVariables>;
+export const GenerateMenuDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GenerateMenu"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"recipes"}},"type":{"kind":"NonNullType","type":{"kind":"ListType","type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"RecipeInput"}}}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"generateMenu"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"recipes"},"value":{"kind":"Variable","name":{"kind":"Name","value":"recipes"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"courses"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"description"}}]}},{"kind":"Field","name":{"kind":"Name","value":"backgroundImage"}}]}}]}}]} as unknown as DocumentNode<GenerateMenuQuery, GenerateMenuQueryVariables>;
